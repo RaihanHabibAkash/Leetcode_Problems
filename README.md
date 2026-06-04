95eb9094eca7b8c4ec93baad5e2d7ccf954ffbdb
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-06-04 13:24:41 +0600

    Sum of Left Leaves(Easy) solved using recurtion

--- README.md
+++ README.md
@@ -88,3 +88,5 @@
 [Soluction 145(Easy)](https://leetcode.com/problems/binary-tree-postorder-traversal/)
 
 [Solution 94(Easy)](https://leetcode.com/problems/binary-tree-inorder-traversal/)
+
+[Solution 404(Easy)](https://leetcode.com/problems/sum-of-left-leaves/)
---
+++ binary_tree/p404_Sum_of_Left_Leaves.cpp
@@ -0,0 +1,60 @@
+// Problem: Sum of Left Leaves
+// Platform: LeetCode
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/sum-of-left-leaves/
+// Topics: Binary Tree, Recurtion
+/*
+Given the root of a binary tree, return the sum of all left leaves.
+
+A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+Example 2:
+
+Input: root = [1]
+Output: 0
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-1000 <= Node.val <= 1000
+*/
+
+
+class Solution {
+public:
+    int left_leaf_sum(TreeNode* root, bool flag) {
+        // Base case
+        if(root == NULL) return 0;
+        if(!root->left && !root->right && flag) return root->val;
+
+        int l = left_leaf_sum(root->left, flag = true);
+        int r = left_leaf_sum(root->right, flag = false);
+
+        return l+r;
+    }
+    int sumOfLeftLeaves(TreeNode* root) {
+        bool flag = false;
+        return left_leaf_sum(root, flag);
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
