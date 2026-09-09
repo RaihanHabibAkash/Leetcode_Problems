744d5eeaa4640482e96fe1098219b9f8494765c3
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-09-09 15:55:24 +0600

    Function updated by Constructor call

--- Dynamic_Programming/p509_Fibonacci_Number.cpp
+++ Dynamic_Programming/p509_Fibonacci_Number.cpp
@@ -38,18 +38,17 @@ class Solution {
 public:
     int dp[35];
 
-    int fibo(int n) {
+    Solution() {
+        memset(dp, -1, sizeof(dp));
+    }
+
+    int fib(int n) {
         if(n < 2)
             return n;
             
         if(dp[n] != -1)
             return dp[n];
 
-        return dp[n] = fibo(n - 1) + fibo(n - 2);
-    }
-
-    int fib(int n) {
-        memset(dp, -1, sizeof(dp));
-        return fibo(n);
+        return dp[n] = fib(n - 1) + fib(n - 2);
     }
 };
